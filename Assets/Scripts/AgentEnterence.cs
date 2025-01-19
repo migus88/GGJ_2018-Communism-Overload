ed580d29021cc54db0e607a0b24a936269c6cc1d
Yuri Sokolov <migsil88@gmail.com>
2025-01-19 18:08:47 +0200

    fixed agent enterance

--- Assets/Scripts/AgentEnterence.cs
+++ Assets/Scripts/AgentEnterence.cs
@@ -1,11 +1,13 @@
 ﻿using migs.EventSystem;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class AgentEnterence : BaseBehaviour
 {
+    [SerializeField] private Vector3 _startingPosition;
+    [SerializeField] private Vector3 _destination;
+    
+    
     private void Start()
     {
         this._typeEvents.Add(typeof(GameStatePayload), EventsManager.Instance.Subscribe<GameStatePayload>(this.onGameStateChanged));
@@ -21,13 +23,14 @@ public class AgentEnterence : BaseBehaviour
 
     private IEnumerator animateEntrence()
     {
-        for (int i = 0; i < 6; i++)
+        // create movement from starting position to destination in 6 seconds
+        var time = 0f;
+        
+        while (time < 6)
         {
-            for (float y = 0; y < 1f; y += Time.deltaTime)
-            {
-                transform.position = new Vector3(transform.position.x + 1f * Time.deltaTime, transform.position.y);
-                yield return new WaitForEndOfFrame();
-            }
+            time += Time.deltaTime;
+            this.transform.position = Vector3.Lerp(this._startingPosition, this._destination, time / 6);
+            yield return null;
         }
     }
 }
